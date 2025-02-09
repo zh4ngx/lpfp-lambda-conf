f9375c3f94fed1322665d67071ec3bcc07e6e634
EstebanMarin <estebanmarin002@gmail.com>
2025-02-08 23:12:36 -0500

    working but falling to animate, testing

--- src/Plotting.hs
+++ src/Plotting.hs
@@ -1,6 +1,7 @@
 module Plotting where
 
 import Graphics.Gnuplot.Simple
+import Graphics.Gloss
 
 type R = Double
 
@@ -14,3 +15,12 @@ yPos y0 vy0 ay t = y0 + vy0*t + 0.5*ay*t^2
 
 plot1 = plotFunc [
     Title "y(t)", XLabel "t", YLabel "y"] tRange (yPos 0 20 (-9.8))
+
+displayMode :: Display
+displayMode = InWindow "Plot" (800, 600) (10, 10)
+
+axes :: Picture
+axes = Pictures [Color red $ Line [(0,0),(100,0)], 
+                 Color green $ Line [(0,0),(0,100)]]
+                
+displayPlot = display displayMode black axes
