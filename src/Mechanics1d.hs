98de74c13292b0cacb9c9fe10fff5bddf7ca2adf
EstebanMarin <estebanmarin002@gmail.com>
2025-02-11 18:58:19 -0500

    add: implement Euler integration and state update functions for 1D mechanics

--- src/Mechanics1d.hs
+++ src/Mechanics1d.hs
@@ -22,3 +22,107 @@ newtonSecond1D m fs (t,x0,v0)
     = let fNet = sum [f (t,x0,v0) | f <- fs]
           acc = fNet / m
       in (1,v0,acc)
+
+euler1D :: R                     -- time step dt
+        -> (State1D -> (R,R,R))  -- differential equation
+        -> State1D -> State1D    -- state-update function
+euler1D dt deriv (t0,x0,v0)
+    = let (_, _, dvdt) = deriv (t0,x0,v0)
+          t1 = t0 + dt
+          x1 = x0 + v0 * dt
+          v1 = v0 + dvdt * dt
+      in (t1,x1,v1)
+
+updateTXV :: R                   -- time interval dt
+          -> Mass
+          -> [State1D -> Force]  -- list of force funcs
+          -> State1D -> State1D  -- state-update function
+updateTXV dt m fs = euler1D dt (newtonSecond1D m fs)
+
+
+statesTXV :: R                   -- time step
+          -> Mass
+          -> State1D             -- initial state
+          -> [State1D -> Force]  -- list of force funcs
+          -> [State1D]           -- infinite list of states
+statesTXV dt m txv0 fs = iterate (updateTXV dt m fs) txv0
+
+-- assume that dt is the same between adjacent pairs
+velocity1D :: [State1D]           -- infinite list
+           -> Time -> Velocity    -- velocity function
+velocity1D sts t
+    = let (t0,_,_) = sts !! 0
+          (t1,_,_) = sts !! 1
+          dt = t1 - t0
+          numSteps = abs $ round (t / dt)
+          (_,_,v0) = sts !! numSteps
+      in v0
+
+velocityFtxv :: R                   -- time step
+             -> Mass
+             -> State1D             -- initial state
+             -> [State1D -> Force]  -- list of force funcs
+             -> Time -> Velocity    -- velocity function
+velocityFtxv dt m txv0 fs = velocity1D (statesTXV dt m txv0 fs)
+
+-- assume that dt is the same between adjacent pairs
+position1D :: [State1D]           -- infinite list
+           -> Time -> Position    -- position function
+position1D sts t
+    = let (t0,_,_) = sts !! 0
+          (t1,_,_) = sts !! 1
+          dt = t1 - t0
+          numSteps = abs $ round (t / dt)
+          (_,x0,_) = sts !! numSteps
+      in x0
+
+positionFtxv :: R                   -- time step
+             -> Mass
+             -> State1D             -- initial state
+             -> [State1D -> Force]  -- list of force funcs
+             -> Time -> Position    -- position function
+positionFtxv dt m txv0 fs = position1D (statesTXV dt m txv0 fs)
+
+springForce :: R -> State1D -> Force
+springForce k (_,x0,_) = -k * x0
+
+dampedHOForces :: [State1D -> Force]
+dampedHOForces = [springForce 0.8
+                 ,\(_,_,v0) -> fAir 2 1.225 (pi * 0.02**2) v0
+                 ,\_ -> -0.0027 * 9.80665
+                 ]
+
+dampedHOStates :: [State1D]
+dampedHOStates = statesTXV 0.001 0.0027 (0.0,0.1,0.0) dampedHOForces
+
+dampedHOGraph :: IO ()
+dampedHOGraph
+    = plotPath [Title "Ping Pong Ball on a Slinky"
+               ,XLabel "Time (s)"
+               ,YLabel "Position (m)"
+            --    ,PNG "dho.png"
+            --    ,Key Nothing
+               ] [(t,x) | (t,x,_) <- take 3000 dampedHOStates]
+
+pingpongPosition :: Time -> Position
+pingpongPosition = positionFtxv 0.001 0.0027 (0,0.1,0) dampedHOForces
+
+dampedHOGraph2 :: IO ()
+dampedHOGraph2
+    = plotFunc [Title "Ping Pong Ball on a Slinky"
+               ,XLabel "Time (s)"
+               ,YLabel "Position (m)"
+               ,Key Nothing
+               ] [0,0.01..3] pingpongPosition
+
+pingpongVelocity :: Time -> Velocity
+pingpongVelocity = velocityFtxv 0.001 0.0027 (0,0.1,0) dampedHOForces
+
+dampedHOGraph3 :: IO ()
+dampedHOGraph3
+    = plotFunc [Title "Ping Pong Ball on a Slinky"
+               ,XLabel "Time (s)"
+               ,YLabel "Velocity (m/s)"
+            --    ,PNG "dho2.png"
+            --    ,Key Nothing
+               ] [0,0.01..3] pingpongVelocity
